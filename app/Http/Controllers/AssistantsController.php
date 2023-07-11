7132ba630b75f722989bad1aa71c933ce59b9381
unknown <eagila926@gmail.com>
2023-07-11 14:19:59 -0500

    proyecot

---
+++ app/Http/Controllers/AssistantsController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Assistants;
+use Illuminate\Http\Request;
+
+class AssistantsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        // Obtener el contenido del código QR (depende de la librería utilizada)
+
+        $content = $request->input('qr_content');
+
+        // Crear una instancia del modelo QrCode
+
+        $qrCode = new QrCode();
+        $qrCode->content = $content;
+        $qrCode->save();
+
+        // Opcional: redirigir o devolver una respuesta
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Assistants  $assistants
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Assistants $assistants)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Assistants  $assistants
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Assistants $assistants)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Assistants  $assistants
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Assistants $assistants)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Assistants  $assistants
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Assistants $assistants)
+    {
+        //
+    }
+}
